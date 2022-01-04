7f4d21801751f034ccdb90190fc35e29539eba3e
Tanveer Ansari <35213794+ansariTanveer@users.noreply.github.com>
2022-01-04 19:19:21 +0530

    update url and key

--- CloudComputingProject/CloudComputingProject/Utilities/Constants.cs
+++ CloudComputingProject/CloudComputingProject/Utilities/Constants.cs
@@ -2,10 +2,10 @@
 {
     public class Constants
     {
-        public const string FACE_API_KEY = "d3ddee055e3441799afac4fb286388ad";
-        public const string FACE_URL = "https://westus.api.cognitive.microsoft.com/face/v1.0/detect";
+        public const string FACE_API_KEY = "Your_API_Key";
+        public const string FACE_URL = "https://{region_name_selected_while_creating_service}.api.cognitive.microsoft.com/face/v1.0/detect";
 
-        public const string OCR_API_KEY = "d2a80fc8a29c4db6804ce7b6ce47d647";
-        public const string OCR_URL = "https://westus.api.cognitive.microsoft.com/";
+        public const string OCR_API_KEY = "Your_API_Key";
+        public const string OCR_URL = "https://{region_name_selected_while_creating_service}.api.cognitive.microsoft.com/";
     }
 }
